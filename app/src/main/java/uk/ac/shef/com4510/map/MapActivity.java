af66dc2f1e21225673826f318176fcad36672b1d
Owain Parry <oparry1@sheffield.ac.uk>
2018-11-15 10:49:18 +0000

    Made map markers clickable.

--- app/src/main/java/uk/ac/shef/com4510/map/MapActivity.java
+++ app/src/main/java/uk/ac/shef/com4510/map/MapActivity.java
@@ -1,6 +1,9 @@
 package uk.ac.shef.com4510.map;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
@@ -8,12 +11,15 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import uk.ac.shef.com4510.R;
 import uk.ac.shef.com4510.data.Image;
+import uk.ac.shef.com4510.details.DetailsActivity;
 
-public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapActivity extends FragmentActivity
+        implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap map;
     private MapViewModel viewModel;
@@ -33,12 +39,14 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     private void setMapMarkers() {
         viewModel.getImages().observe(this, images -> {
             map.clear();
-            if (images == null) {
-                return;
-            }
+
+            if (images == null) return;
+
             for (Image img : images) {
                 LatLng pos = new LatLng(img.getLatitude(), img.getLongitude());
-                map.addMarker(new MarkerOptions().position(pos).title(img.getTitle()));
+                MarkerOptions options = new MarkerOptions().position(pos).title(img.getTitle());
+                Marker marker = map.addMarker(options);
+                marker.setTag(img.getPath());
             }
         });
     }
@@ -47,5 +55,16 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
         setMapMarkers();
+    }
+
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+        Context context = getApplicationContext();
+        Intent startActivityIntent = new Intent(context, DetailsActivity.class);
+        String path = (String) marker.getTag();
+        startActivityIntent.putExtra("imagePath", path);
+        context.startActivity(startActivityIntent);
+
+        return true;
     }
 }
