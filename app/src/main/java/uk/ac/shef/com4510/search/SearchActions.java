5c89f94a586b1eafd322f3ed13afc1649f41b135
Euan Rochester <euan@rochester.me.uk>
2018-11-15 12:03:52 +0000

    Initial Search ui

---
+++ app/src/main/java/uk/ac/shef/com4510/search/SearchActions.java
@@ -0,0 +1,8 @@
+package uk.ac.shef.com4510.search;
+
+import android.view.View;
+
+public interface SearchActions {
+    void showDatePicker(View sourceView, boolean show);
+    void doSearch();
+}
---
+++ app/src/main/java/uk/ac/shef/com4510/search/SearchViewModel.java
@@ -0,0 +1,20 @@
+package uk.ac.shef.com4510.search;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.MutableLiveData;
+import android.support.annotation.NonNull;
+
+import java.util.Calendar;
+
+public class SearchViewModel extends AndroidViewModel {
+
+    final public MutableLiveData<String> title = new MutableLiveData<>();
+    final public MutableLiveData<String> description = new MutableLiveData<>();
+    final public MutableLiveData<Calendar> date = new MutableLiveData<>();
+    final public MutableLiveData<String> datestring = new MutableLiveData<>();
+
+    public SearchViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
