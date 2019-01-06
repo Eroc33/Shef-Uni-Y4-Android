70d3580558854d3c0e80f33ae59470d77f7d4448
Owain Parry <oparry1@sheffield.ac.uk>
2019-01-06 12:56:53 +0000

    onCreate() in Application now calls super.onCreate()

--- app/src/main/java/uk/ac/shef/com4510/Application.java
+++ app/src/main/java/uk/ac/shef/com4510/Application.java
@@ -12,6 +12,7 @@ public class Application extends android.app.Application {
     }
 
     @Override public void onCreate() {
+        super.onCreate();
         imageDbInstance = Room.databaseBuilder(getApplicationContext(),
                 ImageDatabase.class, "com4510-image-database").build();
     }
